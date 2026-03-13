d2b1bdd583ee21c4f63fc562ca315c8070a9b51b
akilaisuru <akilaisurukahingala@gmail.com>
2026-03-13 23:17:03 +0530

    implemented SwapRequestDTO

---
+++ BackEnd/src/main/java/org/example/backend/dto/SwapRequestDTO.java
@@ -0,0 +1,16 @@
+package org.example.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SwapRequestDTO {
+    private Long id;
+    private Long senderId;
+    private Long receiverId;
+    private String message;
+    private String status; // PENDING, ACCEPTED, REJECTED, COMPLETED
+}
