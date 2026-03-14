fe4bbbbba6483be3c4cc787a5c2ecf8de0b91c56
akilaisuru <akilaisurukahingala@gmail.com>
2026-03-14 20:39:58 +0530

    implemented ReviewDTO

---
+++ BackEnd/src/main/java/org/example/backend/dto/ReviewDTO.java
@@ -0,0 +1,17 @@
+package org.example.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class ReviewDTO {
+    private Long id;
+    private Long reviewerId;
+    private Long revieweeId;
+    private int rating;      // 1-5 Stars
+    private String comment;
+}
